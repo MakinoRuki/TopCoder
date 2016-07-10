c5f3647afb02a989cccad5d5f90d0076611d3e46
MakinoRuki <muye67@gmail.com>
2016-07-10 20:27:53 +0800

    Update SRM679Div2Hard.cpp

--- SRM679Div2Hard.cpp
+++ SRM679Div2Hard.cpp
@@ -3,14 +3,37 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
+#include <set>
 #define N 102
 #define M 2005
+#define inf 1000000000
 using namespace std;
 class ForbiddenStreets {
   public:
     int m;
     set<int> path[N][N];
     int dis[N][N];
+    set<int> setIntersection (set<int> & a, set<int> & b) {
+    	set<int> res;
+    	res.clear();
+    	for (set<int>::iterator itr = a.begin(); itr != a.end(); ++itr) {
+    		if (b.find(*itr) != b.end()) {
+    			res.insert(*itr);
+    		}
+    	}
+    	return res;
+    }	
+    set<int> setUnion (set<int> & a, set<int> & b) {
+    	set<int> res;
+    	res.clear();
+    	for (int x : a) {
+    		res.insert(x);
+    	}
+    	for (int y : b) {
+    		res.insert(y);
+    	}	
+    	return res;
+    }
     vector<int> find (int n, vector<int> A, vector<int> B, vector<int> time) {
       m = A.size();
       for (int i = 0; i < n; ++i) {
@@ -28,9 +51,10 @@ class ForbiddenStreets {
           for (int j = 0; j < n; ++j) {
             if (dis[i][j] > dis[i][k] + dis[k][j]) {
               dis[i][j] = dis[i][k] + dis[k][j];
-              path[i][j] = set_union(path[i][k], path[k][j]);
+              path[i][j] = setUnion(path[i][k], path[k][j]);
             } else if (dis[i][j] == dis[i][k] + dis[k][j]) {
-              path[i][j] = set_intersection(path[i][j], set_union(path[i][k], path[k][j]));
+              set<int> res = setUnion(path[i][k], path[k][j]);
+              path[i][j] = setIntersection(path[i][j], res);
             }
           }
         }
