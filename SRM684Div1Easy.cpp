06d264a09a41901c722f90f6a4db7217e264e7cd
MakinoRuki <muye67@gmail.com>
2016-07-19 14:37:02 +0800

    Update SRM684Div1Easy.cpp

--- SRM684Div1Easy.cpp
+++ SRM684Div1Easy.cpp
@@ -5,4 +5,26 @@
 #include <algorithm>
 using namespace std;
 class CliqueParty {
+  public:
+    int n;
+    int maxsize(vector<int> a, int k) {
+      n = a.size();
+      sort(a.begin(), a.end());
+      int ans = 2;
+      for (int b = 0; b < n; ++b) {
+        for (int e = b + 1; e < n; ++e) {
+          int pre = b;
+          int tot = 2;
+          int delta = a[e] - a[b];
+          for (int i = b + 1; i < e; ++i) {
+            if ((a[i] - a[pre]) >= k * delta) {
+              pre = i;
+              tot++;
+            }
+          }
+          ans = max(ans, tot);
+        }
+      }
+      return ans;
+    }
 };
