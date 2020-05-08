cbbe20e5b126d8d9528879bb33c7a7ab0db3646a
MakinoRuki <muye67@gmail.com>
2020-05-07 20:02:03 -0700

    Update SRM606Div1Easy.cpp

--- SRM606Div1Easy.cpp
+++ SRM606Div1Easy.cpp
@@ -8,9 +8,31 @@ using namespace std;
 class EllysNumberGuessing{
 	public:
 		int n;
-		int getNumber(vector<int>guesses, vector<int>answers){
-			n=guesses.size();
-			set<int>tmp;
+		int getNumber(vector<int>gs, vector<int>as){
+			n=gs.size();
+            vector<int> ans;
+            if (gs[0]-as[0]>=1 && gs[0]-as[0] <= 1000000000) {
+            	ans.push_back(gs[0]-as[0]);
+            }
+            if (gs[0]+as[0]>=1 && gs[0]+as[0] <= 1000000000) {
+            	ans.push_back(gs[0]+as[0]);
+            }
+            int cnt=0;
+            int cur=-1;
+            for (int i = 0; i < ans.size(); ++i) {
+            	int j;
+                for (j = 0; j < n; ++j) {
+                	if (ans[i] != gs[j]+as[j] && ans[i] != gs[j]-as[j]) break;
+                }
+                if (j >= n) {
+                    cnt++;
+                    cur=ans[i];
+                }
+            }
+            if (cnt==0) return -2;
+            if (cnt==1) return cur;
+            return -1;
+		/*	set<int>tmp;
 			for(int i=0;i<n;++i){
 				if(i==0){
 					if(guesses[i]-answers[i]>=1)
@@ -44,6 +66,6 @@ class EllysNumberGuessing{
 			else{
 				if(tmp.size()==1)return *tmp.begin();
 				else return -1;
-			}
+			} */
 		}
 };
